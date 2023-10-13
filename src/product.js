3d3a68744046540ab52842b9a6869bf44a5d0b4e
KianIsNotAvailable <kianxw@gmail.com>
2023-10-13 20:50:30 +0100

    first commit

---
+++ src/product.js
@@ -0,0 +1,31 @@
+import product1 from './assets/1.jpeg';
+import product2 from './assets/2.webp';
+import product3 from './assets/3.jpg';
+import product4 from './assets/4.jpg';
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'Dog Food',
+        Price: 2.5,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: 'Dog Bed',
+        Price: 10.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: 'Dog lead',
+        Price: 7.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: 'Dog Coat',
+        Price: 15.0,
+        productImage: product4,
+    },
+]
